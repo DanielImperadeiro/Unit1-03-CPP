5045fb1c82fa78fd2b43bae9baae848d50679548
Ubuntu <ubuntu@ip-172-31-15-252.ec2.internal>
2022-02-24 11:04:16 +0000

    Added code

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Daniel Imperadeiro All rights reserved
+//
+// Created by Daniel Imperadeiro
+// Created on Feb, 22, 2022
+// This is a "Hello world" program with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
